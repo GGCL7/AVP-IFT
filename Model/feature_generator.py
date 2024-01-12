ea16a49273da0216651742712ea9a434e0a8a8f2
GGCL7 <113498910+GGCL7@users.noreply.github.com>
2024-01-12 15:39:14 +0800

    Update feature_generator.py

--- Model/feature_generator.py
+++ Model/feature_generator.py
@@ -7,41 +7,7 @@ from torch.utils.data import Dataset
 import numpy as np
 
 
-# def feature_generator(file_path, temp_file_path):
-#     f = open(file_path, 'r', encoding='utf-8')
-#     fasta_list = np.array(f.readlines())
-#     aa_feature_list = []
-#     for flag in range(0, len(fasta_list), 2):
-#         fasta_str = [[fasta_list[flag].strip('\n').strip(), fasta_list[flag + 1].strip('\n').strip()]]
-#         bin_output = BINARY(fasta_str)
-#         blo_output = BLOSUM62(fasta_str)
-#         zsl_output = ZSCALE(fasta_str)
-#         if 'pos' in bin_output[1][0].split('>')[1]:
-#             feature_id = 1
-#         else:
-#             feature_id = 0
-#         bin_output[1].remove(bin_output[1][0])
-#         blo_output[1].remove(blo_output[1][0])
-#         zsl_output[1].remove(zsl_output[1][0])
-#         bin_feature = []
-#         blo_feature = []
-#         zsl_feature = []
-#         for i in range(0, len(bin_output[1]), 20):
-#             temp = bin_output[1][i:i + 20]
-#             bin_feature.append(temp)
-#         for i in range(0, len(blo_output[1]), 20):
-#             temp = blo_output[1][i:i + 20]
-#             blo_feature.append(temp)
-#         for i in range(0, len(zsl_output[1]), 5):
-#             temp = zsl_output[1][i:i + 5]
-#             zsl_feature.append(temp)
-#         aa_fea_matrx = np.hstack([np.array(bin_feature), np.array(blo_feature), np.array(zsl_feature)])
-#         aa_feature_list.append([feature_id, aa_fea_matrx])
-#     aa_feature_list = np.array(aa_feature_list, dtype=object)
-#     print(aa_feature_list)
-#     # np.save(temp_file_path, aa_feature_list)
-#     # return temp_file_path+'.npy'
-#     # return aa_feature_list
+
 import torch
 import numpy as np
 import os
